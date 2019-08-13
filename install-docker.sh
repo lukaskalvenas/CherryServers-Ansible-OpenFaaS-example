827c9bf6185dcbba328bc26fd146d6c0034f42bf
lukaskalvenas <54058403+lukaskalvenas@users.noreply.github.com>
2019-08-13 12:23:59 +0300

    Delete install-docker.sh

--- install-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
